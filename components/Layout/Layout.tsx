af74c76ffdcd2b4b5dc328ac95f11639eb1e71b6
Mattie Lee <hello@mattiel.dev>
2022-01-10 03:02:06 -0800

    added footer

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '@components/Navbar';
+import Link from 'next/link'
 
 const Layout: React.FC = (props) => {
   const { children } = props;
@@ -7,6 +8,35 @@ const Layout: React.FC = (props) => {
     <main className="layout w-full h-full relative">
       <Navbar />
       {children}
+      <footer className="mt-16 pb-12">
+        <hr className="dark:border-gray-900 border-gray-50 pb-8" />
+        <div className="grid grid-cols-1 md:grid-cols-2 place-items-start">
+          <h6 className="text-sm">© 2022 Mattie Lee — LA/KR</h6>
+          <div className="flex justify-evenly w-full">
+            <ul className="flex flex-col gap-4 text-gray-700 dark:text-gray-300">
+              <li className="hover:text-white transition-color">
+                <Link href="/">
+                  <a>
+                    Home
+                  </a>
+                </Link>
+              </li>
+            </ul>
+            <ul className="flex flex-col gap-4 text-gray-700 dark:text-gray-300">
+              <li className="hover:text-white transition-color">
+                <a href="https://github.com/mattiel">
+                  Github
+                </a>
+              </li>
+              <li className="hover:text-white transition-color">
+                <a href="https://www.linkedin.com/in/jsmattly/">
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 };
