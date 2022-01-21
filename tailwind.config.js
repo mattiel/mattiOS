f6b3bd2dc29bd9565eacd3f829314dc65f37ee99
Mattie Lee <hello@mattiel.dev>
2022-01-21 12:03:46 -0800

    style update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { spacing } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   mode: 'jit',
   content: [
@@ -5,9 +7,44 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            'h2, h3': {
+              'scroll-margin-top': spacing[24],
+            },
+            hr: {
+              borderColor: 'transparent',
+              height: '1px',
+              borderWidth: 0,
+              background: `linear-gradient(to right, ${theme('colors.gray.50')}, ${theme('colors.transparent')})`,
+            }
+          }
+        },
+        xl: {
+          css: {
+            'h2, h3 > a': {
+              fontWeight: 700,
+            },
+            'h2': {
+              fontSize: '1.25em'
+            },
+            'h3': {
+              fontSize: '1em'
+            },
+            p: {
+              fontSize: '1em'
+            },
+          }
+        },
+        dark: {
+          hr: {
+            background: `linear-gradient(to right, ${theme('colors.gray.900')}, ${theme('colors.transparent')})`,
+          }
+        }
+      })
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require('@tailwindcss/typography')],
 }
