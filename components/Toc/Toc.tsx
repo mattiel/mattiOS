54f7d628cc495061e8b1424f9e39f00257592f86
Mattie Lee <hello@mattiel.dev>
2022-02-09 13:33:25 -0800

    Section highlighting (not working yet)

--- components/Toc/Toc.tsx
+++ components/Toc/Toc.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {ReactElement, Ref, RefObject} from 'react';
 import type { Project } from '.contentlayer/types'
 import clsx from "clsx";
 
 const Toc = ({ project }: { project: Project }) => {
-  const parse = () => {
+  const [slugs, setSlugs] = React.useState(Array<string>());
+  const [activeSlug, setActiveSlug] = React.useState('')
+
+  function parse() {
     return project.body.raw.split('\n').filter((line) => line.startsWith('#'))
   }
+
+  const Heading = ({ href, title }: {href: string, title: string}) => {
+    return(
+      <a
+        href={href}
+        className={clsx(
+          "leading-relaxed",
+          activeSlug === href ? "dark:text-white dark:hover:text-gray-300" : "dark:text-gray-400 dark:hover:text-white"
+        )}
+      >
+        {title}
+      </a>
+    )
+
+  }
+
+  function handleEntries(entries: IntersectionObserverEntry[]) {
+    entries.forEach(entry => {
+      entry.isIntersecting && setActiveSlug(entry.target.getAttribute('id') as string)
+      console.log(entry.target, entry.isIntersecting, activeSlug)
+    })
+  }
+
+  React.useEffect(() => {
+    const target = document.body.querySelector('article.prose')
+    if (target) {
+      setSlugs(Array.from(target.querySelectorAll('h2[id], h3[id]')).map(el => { return el.id }))
+      const observer = new IntersectionObserver(handleEntries, {
+        root: document.body,
+        threshold: 1
+      })
+      Array.from(target.querySelectorAll('h2[id], h3[id]')).forEach(element => observer.observe(element))
+      return () => observer.disconnect()
+    }
+
+  },[])
 
   return (
     <aside className="hidden 2xl:block relative px-12 h-full w-full overflow-hidden max-w-xs">
@@ -15,17 +54,16 @@ const Toc = ({ project }: { project: Project }) => {
         </p>
         <ul className="">
           {
+            // Slugs are currently generated from the title, doesn't consider duplicate title edge cases yet.
             parse().map((line, i) => {
               const isSubHeader = line.startsWith('###')
               const [, title] = line.split('# ')
               return (
                 <li key={i} className={clsx("text-sm", isSubHeader && 'ml-3')}>
-                  <a
+                  <Heading
                     href={`#${title?.toLowerCase().replace(/ /g, '-')}`}
-                    className="dark:text-gray-400 dark:hover:text-white leading-relaxed"
-                  >
-                    {title}
-                  </a>
+                    title={title}
+                  />
                 </li>
               )
             })
