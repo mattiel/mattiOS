cc0c6a312df5ff5ac6f8ae9b70baaa762c4abae7
Mattie Lee <hello@mattiel.dev>
2022-02-09 13:31:39 -0800

    Styling changes

--- components/Video/Video.tsx
+++ components/Video/Video.tsx
@@ -15,7 +15,7 @@ export enum VideoType {
 
 const Video: React.FC<VideoProps> = ({ src, width, height, type }) => {
   return (
-    <div className="w-full p-8 flex justify-center dark:bg-zinc-900 bg-zinc-50">
+    <div className="video w-full mt-[2em] p-8 flex justify-center dark:bg-zinc-700 bg-zinc-400">
       <video className="rounded-[1.25rem] overflow-hidden" controls={false} playsInline autoPlay muted loop width={width} height={height}>
         <source src={src} type={`video/${type}`} />
       </video>
